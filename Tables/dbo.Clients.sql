84fe9b61040cfd95ec59de8bcba5ef3bb0c85d81
Rida Fadil <157375930+RIDAFADIL@users.noreply.github.com>
2024-02-13 15:34:19 +0100

    xdg

---
+++ Tables/dbo.Clients.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Clients] (
+  [ID] [int] NOT NULL,
+  [Name] [nvarchar](50) NOT NULL,
+  [Age] [tinyint] NOT NULL,
+  CONSTRAINT [PK_Clients] PRIMARY KEY CLUSTERED ([ID])
+)
+ON [PRIMARY]
+GO
