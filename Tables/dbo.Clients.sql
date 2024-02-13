99d67dd9ba7c4f28356207bd1b128c9f0497acdb
Rida Fadil <157375930+RIDAFADIL@users.noreply.github.com>
2024-02-13 15:38:10 +0100

    ertdgfg

--- Tables/dbo.Clients.sql
+++ Tables/dbo.Clients.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[Clients] (
-  [ID] [int] NOT NULL,
-  [Name] [nvarchar](50) NOT NULL,
+  [ID] [int] IDENTITY,
+  [Nom] [nvarchar](50) NOT NULL,
   [Age] [tinyint] NOT NULL,
+  [Type] [nvarchar](50) NOT NULL,
   CONSTRAINT [PK_Clients] PRIMARY KEY CLUSTERED ([ID])
 )
 ON [PRIMARY]
